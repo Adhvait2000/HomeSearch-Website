7f92736255a8e8dcce540d229137a6a0eca9d01e
Zhi Yang <tohzhiyangrv@gmail.com>
2023-02-19 22:38:49 +0800

    Setting security headers and prevent XSS attacks

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ const colors = require("colors");
 const fileupload = require("express-fileupload");
 const cookieParser = require("cookie-parser");
 const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
 const errorHandler = require("./middleware/error");
 const connectDB = require("./config/db");
 
@@ -30,6 +32,12 @@ app.use(fileupload());
 
 // Sanitize data
 app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
 
 // Mount routers
 app.use("/homesearch/v1/auth", auth);
