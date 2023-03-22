1074c104dc38bd38d6b2f499510b9223a16b554c
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2023-03-22 15:51:50 +0800

    updates watchlist

--- backend/branch-edit/controllerwatchlist.js
+++ backend/branch-edit/controllerwatchlist.js
@@ -2,13 +2,22 @@ const mongoose = require('mongoose');
 const User = require('../models/User');
 
 
-exports.getFullWatchList = (req,res,next) => {
+exports.getFullWatchList = async (req,res,next) => {
+    const userId = req.params.userId ;
+    try {
+        const userData = await User.findOne({_id: userId});
+        res.json(userData.watchlist);
+    }catch (err) {
+        res.status(500).send(err)  ;
 
+    }
 };
 
 
 exports.retrieveHousingDev = (req,res,next) => {
-
+    const userId = req.params.userId ;
+    const itemId = req.params.itemId;
+    
 };
 
 exports.addToWatchlist = (req,res,next) => {
@@ -16,7 +25,24 @@ exports.addToWatchlist = (req,res,next) => {
 };
 
 
-exports.removeFromWatchlist = (req,res,next) => {
+exports.removeFromWatchlist = async (req,res,next) => {
+    const userId = req.params.userId;
+    const itemId = req.params.itemId;
+    
+    try{
+        const userData = await User.findOne({_id: userId});
+        const curWatchlist = userData.watchlist;
+        const watchlistItem = await curWatchlist.find({_id: itemId});
 
+        if(!watchlistItem ){
+            res.status (404).send("Watchlist not found.");
+        }
+        else{
+            await watchlistItem.remove();
+            res.send ("Watchlist iterm removed.");
+        }
+    }catch(err){
+        res.status(500).send(err);
+    }
 };
 
--- backend/branch-edit/routeswatchlist.js
+++ backend/branch-edit/routeswatchlist.js
@@ -4,6 +4,8 @@ const user = require('../models/User');
 const WatchlistController = require('../controllers/watchlist');
 const { protect } = require("../middleware/auth");
 
+//userId is the mongodb id of the user
+// iterm id is the mongodb id of the housing development
 
 router.get('/:userId',protect,WatchlistController.getFullWatchList );
 router.post('/:userId',protect,WatchlistController.addToWatchlist);
