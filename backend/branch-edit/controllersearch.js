30a3f53a33f290afbd459d1834cb5f6149479bc8
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2023-03-23 11:31:43 +0800

    updates

--- backend/branch-edit/controllersearch.js
+++ backend/branch-edit/controllersearch.js
@@ -14,9 +14,10 @@ exports.getAllTownData = async (req,res, next)=>{
 
 exports.getTownStatistics = async(req,res,next)=>{
     try{
+        const districtNum=  req.params.districtNumber ;
+        const items = await TownStats.find({districtNumber: districtNum});
+
         if(Object.keys(req.query).length==0){
-            const districtNum=  req.params.districtNumber ;
-            const items = await TownStats.find({districtNumber: districtNum});
             const results = await MainData.find({districtNumber: districtNum});
             const maxCount = 50;
             const limitedResults  = results.slice(0,maxCount);
@@ -26,8 +27,18 @@ exports.getTownStatistics = async(req,res,next)=>{
     
         }
         else {
+            const { price, rentalPrice,buyPrice ,type, buyOrRent, publicOrPrivate } = req.query;
+            const queryParams = {};
+            if (price) queryParams.price = price;
+            if (rentalPrice) queryParams.rentalPrice = rentalPrice;
+            if (type) queryParams.type = type;
+            if (buyOrRent) queryParams.buyOrRent = buyOrRent;
+            if (publicOrPrivate) queryParams.publicOrPrivate = publicOrPrivate;
+            if(buyPrice)queryParams.buyPrice = buyPrice;
             
-        }
+            const results = await MainData.find(queryParams);
+
+            }
 
 
 
