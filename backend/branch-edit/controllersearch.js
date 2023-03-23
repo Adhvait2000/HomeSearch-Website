c067d461c18946177121ad3cf7491c181c4bb62e
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2023-03-23 12:12:32 +0800

    updates

--- backend/branch-edit/controllersearch.js
+++ backend/branch-edit/controllersearch.js
@@ -21,23 +21,21 @@ exports.getTownStatistics = async(req,res,next)=>{
             const results = await MainData.find({districtNumber: districtNum});
             const maxCount = 50;
             const limitedResults  = results.slice(0,maxCount);
-            const data = { items , limitedResults};
+            const data = { items, limitedResults};
 
             res.json(data);
     
         }
         else {
-            const { price, rentalPrice,buyPrice ,type, buyOrRent, publicOrPrivate } = req.query;
+            const { rentalPrice,buyPrice , buyOrRent, publicOrPrivate } = req.query;
             const queryParams = {};
-            if (price) queryParams.price = price;
             if (rentalPrice) queryParams.rentalPrice = rentalPrice;
-            if (type) queryParams.type = type;
+            if (buyPrice)queryParams.buyPrice = buyPrice;
             if (buyOrRent) queryParams.buyOrRent = buyOrRent;
             if (publicOrPrivate) queryParams.publicOrPrivate = publicOrPrivate;
-            if(buyPrice)queryParams.buyPrice = buyPrice;
             
             const results = await MainData.find(queryParams);
-
+            res.json(results);
             }
 
 
---
+++ backend/branch-edit/routespriceestimator.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const { protect } = require("../middleware/auth");
+
+const PriceEstimatorController = require('../controllers/priceestimator');
+
+router.get('/',protect,PriceEstimatorController.getPrices);
+
+module.exports = router;
