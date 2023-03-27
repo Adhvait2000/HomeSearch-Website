3103a57aaa0d20e8f219bf68a031fd43189ca793
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2023-03-27 19:46:08 +0800

    new dataset

--- backend/branch-edit/controllerpriceestimator.js
+++ backend/branch-edit/controllerpriceestimator.js
@@ -2,7 +2,10 @@ const MainData = require('../models/MainData');
 const TownStats = require('../models/TownStats');
 
 exports.getPrices = async(req, res, next){
-    const = req.query;
-
+    const query = req.query;
     
+    
+
+
+
 }
