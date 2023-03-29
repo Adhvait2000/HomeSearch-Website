ac60f66ed6670a2d360b260fe5b7349185504378
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2023-03-29 18:51:31 +0800

    route, controller update

--- backend/branch-edit/controllerpriceestimator.js
+++ backend/branch-edit/controllerpriceestimator.js
@@ -1,11 +1,37 @@
 const MainData = require('../models/MainData');
 const TownStats = require('../models/TownStats');
-
-exports.getPrices = async(req, res, next){
+const fs = require('fs');
+exports.getPrices = async(req, res, next)=>{
     const query = req.query;
     
     
+};
+
+exports.getPriceEstimatorHome= async(req, res,next)=>{
+    try {
+        const townsList = await TownStats.find({});
+        const 
+    }catch{
+        
+
+    }
+};
+
+exports.getPropertyTypeList = async(req, res, next)=>{
+     
+        fs.readFile('../_data/property_type.json', (err, data)=>{
+            if(err){
+                console.error(err);
+                res.status(500).send('error reding file') ;
+
+            }
+            else {
+                res.setHeader('Content-Type', 'application/json');
+                res.send(data);
+
+            }
+        });
+    
+};
 
 
-
-}
--- backend/branch-edit/routespriceestimator.js
+++ backend/branch-edit/routespriceestimator.js
@@ -2,7 +2,12 @@ const express = require('express');
 const router = express.Router();
 const { protect } = require("../middleware/auth");
 const PriceEstimatorController = require('../controllers/priceestimator');
+const SearchController = require('../controllers/search');
 
-router.get('/',protect,PriceEstimatorController.getPrices);
+router.get('/property-type-list',protect,PriceEstimatorController.getPropertyTypeList);
+router.get('/dropdown-data',protect,SearchController.getAllTownData );
+router.get ('/:districtNumber/:property-type/:public-private', protect,PriceEstimatorController.getEsamite);
+router.get('/:districtNumber', protect, SearchController.getTownStatistics);
+
 
 module.exports = router;
