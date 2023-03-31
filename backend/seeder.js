0a20f5be6d430d7918f2b7f7379440264ab0f38b
Zhi Yang <tohzhiyangrv@gmail.com>
2023-03-31 18:51:46 +0800

    minor changes

--- backend/seeder.js
+++ backend/seeder.js
@@ -8,6 +8,7 @@ dotenv.config({ path: "./config/config.env" });
 
 // Load models
 const User = require("./models/User");
+const MainData = require("./models/MainData");
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -19,15 +20,16 @@ const users = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/users.json`, "utf-8")
 );
 
-// const maindata = JSON.parse(
-//   fs.readFileSync(`${__dirname}/_data/maindata.json`, "utf-8")
-// );
+const maindata = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/trial.json`, "utf-8")
+);
 
 // Import into DB
 const importData = async () => {
   try {
     // await MainData.create(maindata);
     await User.create(users);
+    await MainData.create(maindata);
     console.log("Data Imported...".green.inverse);
     process.exit();
   } catch (error) {
@@ -41,6 +43,7 @@ const deleteData = async () => {
     // await Bootcamp.deleteMany();
     // await MainData.deleteMany();
     await User.deleteMany();
+    await MainData.deleteMany();
     console.log("Data Deleted...".red.inverse);
     process.exit();
   } catch (error) {
