cbd265086eafa87d64619941845346ab0b883066
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2023-04-01 10:31:41 +0800

    routes update

--- backend/routes/priceestimator.js
+++ backend/routes/priceestimator.js
@@ -6,7 +6,7 @@ const SearchController = require('../controllers/search');
 
 router.get('/property-type-list', protect, PriceEstimatorController.getPropertyTypeList);
 router.get('/dropdown-data', protect,SearchController.getAllTownData);
-//router.get ('/estimate-price', protect, PriceEstimatorController.getEstimate);
+router.get ('/estimate-price', protect, PriceEstimatorController.getPrices);
 router.get('/:districtNumber', protect, SearchController.getTownStatistics);
 router.get('/estimate-price/:districtNumber', protect, PriceEstimatorController.getDistrictTypePrices)
 
